f250cd6305f8ce2127190e79c59027d320b1a052
IkanDongo <noel.535230067@stu.untar.ac.id>
2024-06-19 10:06:39 +0700

    followers progress still 50%

--- social-media/app/Http/Controllers/FollowerController.php
+++ social-media/app/Http/Controllers/FollowerController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Follower;
 
+
 class FollowerController extends Controller
+
+
 {
-    
+    public function follower(User $user)
+    $followerid = Auth::id();
+    $followingid =  $user->id;
+
 }
